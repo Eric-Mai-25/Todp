90f22249378e70d54201279593b753da6a893c96
Eric-Mai-25 <ericmai2@gmail.com>
2024-07-30 12:40:12 -0700

    Making todo tasks

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:todo/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: Text('TODO'),
+        elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          TodoTile(
+            taskName: 'Make bed',
+            taskCompleted: true,
+            onChanged: (p0) {},
+          ),
+          TodoTile(
+            taskName: 'excercise',
+            taskCompleted: false,
+            onChanged: (p0) {},
+          ),
+        ],
+      ),
+    );
+  }
+}
